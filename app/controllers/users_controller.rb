58526091c4f184f6ed53f14063616d425a3a872c
snafkin2222 <78810059+snafkin2222@users.noreply.github.com>
2021-02-28 16:09:47 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,6 @@ class UsersController < ApplicationController
     @users = User.all.page(params[:page])
   end
 
-  def show
-    @user = User.find(params[:id])
-  end
-
   def new
     @user = User.new
   end
